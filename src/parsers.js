996020d0a1a29e7108ab5b97da244a69a976c0b0
Marr <marat.salah1313@gmail.com>
2023-12-17 21:06:01 +0100

    add parser for json

---
+++ src/parsers.js
@@ -0,0 +1,15 @@
+import path from 'node:path'
+import fs from 'node:fs'
+
+export const jsonParser = (filepath1, filepath2) => {
+  const path1 = path.resolve(filepath1);
+  const path2 = path.resolve(filepath2);
+
+  const strOfFile1 = fs.readFileSync(path1);
+  const strOfFile2 = fs.readFileSync(path2);
+
+  const dataOfFile1 = JSON.parse(strOfFile1);
+  const dataOfFile2 = JSON.parse(strOfFile2);
+
+  return [dataOfFile1, dataOfFile2];
+};
